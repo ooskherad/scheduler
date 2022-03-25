0b9e67cb54810f649e19957fdf8df62f9cbb02c2
Amir Hosein <ooskherad@gmail.com>
2022-03-25 13:30:34 +0430

    feat: config  postgres database

--- scheduling/scheduling/settings.py
+++ scheduling/scheduling/settings.py
@@ -72,11 +72,21 @@ WSGI_APPLICATION = 'scheduling.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
