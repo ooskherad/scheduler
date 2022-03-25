b24f2e63acdd94657431b2103946cd71afedbca5
Amir Hosein <ooskherad@gmail.com>
2022-03-25 15:36:37 +0430

    feat: install apss to setting

--- scheduling/scheduling/settings.py
+++ scheduling/scheduling/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'reservation.apps.ReservationConfig',
+    'scheduler.apps.SchedulerConfig',
 ]
 
 MIDDLEWARE = [
